b86c5e5e368df6d001dd23bdacbd8f7f633ac6f2
Robert Garner <rgarner011235@gmail.com>
2024-09-03 18:26:24 -0600

    Added table

--- CPPToCSharpDemos/Program.cs
+++ CPPToCSharpDemos/Program.cs
@@ -5,12 +5,30 @@
         static void Main(string[] args)
         {
             //get name
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
             //say hello to name
 
-            Console.WriteLine("Hello, "+name+"!");
+            //Console.WriteLine("Hello, "+name+"!");
 
+
+            //display numbers and strings
+            decimal cost = 12.45M;
+            int index = 5;
+            Console.WriteLine("The cost is {0:C} and the index is {1}.", cost, index);
+            Console.WriteLine($"The cost is {cost:C} and the index is {index}.");
+            string text = $"The cost is {cost:C} and the index is {index}.";
+
+            int[] numbers = { 100, 200, 300, 400 };
+            string[] names = { "Bob", "Susan", "Tom", "Annita" };
+            Console.WriteLine($"{"index",-10} {"name",-20} {"number",-5}");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                //Console.WriteLine(i.ToString()+names[i]+numbers[i]);
+                Console.WriteLine($"{i,-10} {names[i],-20} {numbers[i],-5}");
+            }
+
+            Console.WriteLine(text);
             //wait for user to acknowledge the results
             Console.WriteLine("Press Enter to terminate...");
             Console.Read();
